bc363262c7e54ed37950275295901d3b1358623a
Rahul <rahulchalat@yahoo.com>
2022-10-06 18:49:57 +0530

    Iniiial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
+import Coins from "./components/Coins";
+import Coin from "./routes/Coin";
+import Navbar from "./components/Navbar";
+
+function App() {
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false";
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setCoins(response.data);
+        console.log(response.data[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Coins coins={coins} />} />
+        <Route path="/coin" element={<Coin />}>
+          <Route path=":coinid" element={<Coin />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Coins.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Coinitem from "./Coinitem";
+import { Link } from "react-router-dom";
+import Coin from "../routes/Coin";
+
+const Coins = (props) => {
+  return (
+    <div className="Container">
+      <div>
+        <div className="heading">
+          <p>#</p>
+          <p className="coin-name">Coins</p>
+          <p>Price</p>
+          <p>24h</p>
+          <p className="hide-mobile">Volume</p>
+          <p className="hide-mobile">Mkt cap</p>
+        </div>
+        {props.coins.map((coins) => {
+          return (
+            <Link to={`/coin/${coins.id}`} element={<Coin />} key={coins.id}>
+              <Coinitem coins={coins} key={coins.id} />;
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Coins;
